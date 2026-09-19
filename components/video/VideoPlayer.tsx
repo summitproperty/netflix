8bcc7fa9b4ac9ab012909dd87cddfdfaae9d7e1f
MD SHAHRIAR NAFIZ <telegram50pic@gmail.com>
2026-09-19 17:50:24 +0600

    Update VideoPlayer.tsx

--- components/video/VideoPlayer.tsx
+++ components/video/VideoPlayer.tsx
@@ -187,7 +187,7 @@ export function VideoPlayer({
          <iframe
   key={attempt}
   src={source.embedUrl}
-  title={${title} player}
+  title={`${title} player`}
   className="absolute inset-0 h-full w-full"
   sandbox="allow-scripts allow-same-origin allow-presentation allow-forms"
   allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
